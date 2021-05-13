4e1da75e1ad7546e9381f03c88db20c801879a97
瑞杰 许 <rjxu@mail.sdu.edu.cn>
2021-05-13 15:30:29 +0800

    Added OpenGL context class.

--- Fountain/src/Platform/Windows/WindowsWindow.cpp
+++ Fountain/src/Platform/Windows/WindowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "Fountain/Events/MouseEvent.h"
 #include "Fountain/Events/KeyEvent.h"
 
+#include "Platform/OpenGL/OpenGLContext.h"
+
 #include <glad/glad.h>
 
 namespace Fountain {
@@ -49,9 +51,10 @@ namespace Fountain {
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
-		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-		FT_CORE_ASSERT(status, "Failed to initialized Glad!");
+
+		m_Context = new OpenGLContext(m_Window);
+		m_Context->Init();
+
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
@@ -150,7 +153,7 @@ namespace Fountain {
 	void WindowsWindow::OnUpdate()
 	{
 		glfwPollEvents();
-		glfwSwapBuffers(m_Window);
+		m_Context->SwapBuffers();
 	}
 
 	void WindowsWindow::SetVSync(bool enabled)
--- Fountain/src/Platform/Windows/WindowsWindow.h
+++ Fountain/src/Platform/Windows/WindowsWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Fountain/Window.h"
+#include "Fountain/Renderer/GraphicsContext.h"
 
 #include <GLFW/glfw3.h>
 
@@ -28,6 +29,7 @@ namespace Fountain {
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
 
 		struct WindowData
 		{
