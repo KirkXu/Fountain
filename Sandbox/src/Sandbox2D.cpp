4e76fc42fb9dbb3cfaaa5a9696590d34e31aec75
瑞杰 许 <rjxu@mail.sdu.edu.cn>
2021-05-30 22:35:28 +0800

    Added transforms and to Renderer2D.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -31,11 +31,9 @@ void Sandbox2D::OnUpdate(Fountain::Timestep ts)
 	Fountain::RenderCommand::Clear();
 
 	Fountain::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Fountain::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Fountain::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Fountain::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 	Fountain::Renderer2D::EndScene();
-	// TODO: Add these functions - Shader::SetMat4, Shader::SetFloat4
-	// std::dynamic_pointer_cast<Fountain::OpenGLShader>(m_FlatColorShader)->Bind();
-	// std::dynamic_pointer_cast<Fountain::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 void Sandbox2D::OnImGuiRender()
