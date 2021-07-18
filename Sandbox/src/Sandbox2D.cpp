0584a6bf552f7ad75ed31b3563876a30278b45c9
瑞杰 许 <rjxu@mail.sdu.edu.cn>
2021-07-18 17:51:33 +0800

    Renderer2D: added draw call splitting and stats.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -29,6 +29,7 @@ void Sandbox2D::OnUpdate(Fountain::Timestep ts)
 	m_CameraController.OnUpdate(ts);
 
 	// Render
+	Fountain::Renderer2D::ResetStats();
 	{
 		FT_PROFILE_SCOPE("Renderer Prep");
 		Fountain::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
@@ -44,8 +45,19 @@ void Sandbox2D::OnUpdate(Fountain::Timestep ts)
 		Fountain::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Fountain::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Fountain::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Fountain::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.f }, m_CloudTexture, 10.0f);
+		Fountain::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.f }, m_CloudTexture, 10.0f);
 		Fountain::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CloudTexture, 20.0f);
+		Fountain::Renderer2D::EndScene();
+
+		Fountain::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 1.0f };
+				Fountain::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+			}
+		}
 		Fountain::Renderer2D::EndScene();
 	}
 }
@@ -55,6 +67,14 @@ void Sandbox2D::OnImGuiRender()
 	FT_PROFILE_FUNCTION();
 	
 	ImGui::Begin("Settings");
+
+	auto stats = Fountain::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
 	ImGui::End();
