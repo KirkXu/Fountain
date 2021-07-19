e6721c3f324e889da9c13f135335018bce450b73
瑞杰 许 <rjxu@mail.sdu.edu.cn>
2021-07-19 16:15:35 +0800

    Fixed some bugs.

--- Fountain/src/Platform/OpenGL/OpenGLVertexArray.h
+++ Fountain/src/Platform/OpenGL/OpenGLVertexArray.h
@@ -20,6 +20,7 @@ namespace Fountain {
 		virtual const Ref<IndexBuffer>& GetIndexBuffer() const { return m_IndexBuffers; }
 	private:
 		uint32_t m_RendererID;
+		uint32_t m_VertexBufferIndex = 0;
 		std::vector<Ref<VertexBuffer>> m_VertexBuffers;
 		Ref<IndexBuffer> m_IndexBuffers;
 	};
