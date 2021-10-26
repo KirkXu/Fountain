fd910f550f0e2b7336e47608a874bb742ab43dce
KirkXu <rjxu@mail.sdu.edu.cn>
2021-10-26 11:16:46 +0800

    Refactored Input "situation"

--- Fountain/src/Fountain/Core/Input.h
+++ Fountain/src/Fountain/Core/Input.h
@@ -8,28 +8,12 @@ namespace Fountain {
 
 	class Input
 	{
-	protected:
-		Input() = default;
 	public:
-		virtual ~Input() = default;
+		static bool IsKeyPressed(KeyCode key);
 
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
-
-		inline static bool IsKeyPressed(KeyCode key) { return s_Instance->IsKeyPressedImpl(key); }
-
-		inline static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		inline static float IsMouseX() { return s_Instance->IsMouseXImpl(); }
-		inline static float IsMouseY() { return s_Instance->IsMouseYImpl(); }
-	protected:
-		virtual bool IsKeyPressedImpl(KeyCode keycode) = 0;
-
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float IsMouseXImpl() = 0;
-		virtual float IsMouseYImpl() = 0;
-	private:
-		static Input* s_Instance;
+		static bool IsMouseButtonPressed(MouseCode button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 }
--- Fountain/src/Platform/Windows/WindowsInput.cpp
+++ Fountain/src/Platform/Windows/WindowsInput.cpp
@@ -1,28 +1,25 @@
 #include "ftpch.h"
-#include "WindowsInput.h"
+#include "Fountain/Core/Input.h"
 
 #include "Fountain/Core/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace Fountain {
-
-	Input* Input::s_Instance = new WindowsInput();
-
-	bool WindowsInput::IsKeyPressedImpl(KeyCode key)
+	bool Input::IsKeyPressed(KeyCode key)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(key));
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button)
+	bool Input::IsMouseButtonPressed(MouseCode button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(button));
 		return state == GLFW_PRESS;
 	}
 
-	std::pair<float, float> WindowsInput::GetMousePositionImpl() 
+	std::pair<float, float> Input::GetMousePosition() 
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
@@ -31,15 +28,15 @@ namespace Fountain {
 		return { (float)xpos, (float)ypos };
 	}
 
-	float WindowsInput::IsMouseXImpl()
+	float Input::GetMouseX()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 		return x;
 	}
 
-	float WindowsInput::IsMouseYImpl()
+	float Input::GetMouseY()
 	{
-		auto[x, y] = GetMousePositionImpl();
+		auto[x, y] = GetMousePosition();
 		return y;
 	}
 
