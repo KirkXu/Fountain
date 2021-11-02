29f626ce867a9d1b790ae7f468ac0fd5b1bf0261
KirkXu <rjxu@mail.sdu.edu.cn>
2021-11-02 14:55:35 +0800

    Multiple small improvements

--- Fountain/src/Fountain/Renderer/Buffer.h
+++ Fountain/src/Fountain/Renderer/Buffer.h
@@ -70,7 +70,7 @@ namespace Fountain {
 	{
 	public:
 		BufferLayout() {}
-		BufferLayout(const std::initializer_list<BufferElement>& elements)
+		BufferLayout(std::initializer_list<BufferElement> elements)
 			: m_Elements(elements) 
 		{
 			CalculateOffsetAndStride();
