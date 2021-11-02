ea34e5975841892fbbc07d0db96dd3d4bd057b65
KirkXu <rjxu@mail.sdu.edu.cn>
2021-11-02 14:22:37 +0800

    Replace filename extraction with equivalent standar library function

--- Fountain/src/Fountain/Core/Assert.h
+++ Fountain/src/Fountain/Core/Assert.h
@@ -2,30 +2,15 @@
 
 #include "Fountain/Core/Base.h"
 #include "Fountain/Core/Log.h"
+#include <filesystem>
 
 #ifdef FT_ENABLE_ASSERTS
-
-	namespace Fountain::Assert
-	{
-		// Returns the simple file name rather than full path as suggested by LovelySanta
-		constexpr const char* CurrentFileName(const char* path) {
-			const char* file = path;
-			while (*path)
-			{
-				if (*path == '/' || *path == '\\')
-					file = ++path;
-				else
-					path++;
-			}
-			return file;
-		}
-	}
 	
 	// Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 	// provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
 	#define FT_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { FT##type##ERROR(msg, __VA_ARGS__); FT_DEBUGBREAK(); } }
 	#define FT_INTERNAL_ASSERT_WITH_MSG(type, check, ...) FT_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-	#define FT_INTERNAL_ASSERT_NO_MSG(type, check) FT_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", FT_STRINGIFY_MACRO(check), ::Fountain::Assert::CurrentFileName(__FILE__), __LINE__)
+	#define FT_INTERNAL_ASSERT_NO_MSG(type, check) FT_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", FT_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 	
 	#define FT_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 	#define FT_INTERNAL_ASSERT_GET_MACRO(...) FT_EXPAND_MACRO( FT_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, FT_INTERNAL_ASSERT_WITH_MSG, FT_INTERNAL_ASSERT_NO_MSG) )
