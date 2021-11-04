a988bb1c116f329f77ba314d611baaf2306b54b6
KirkXu <rjxu@mail.sdu.edu.cn>
2021-11-04 11:07:49 +0800

    Added New, Open, and Save As file menu options to Fountainhead + Win32 open/save file dialogs

--- Fountainhead/src/EditorLayer.h
+++ Fountainhead/src/EditorLayer.h
@@ -18,6 +18,12 @@ namespace Fountain {
 		virtual void OnImGuiRender() override;
 		void OnEvent(Event& e) override;
 	private:
+		bool OnKeyPressed(KeyPressedEvent& e);
+
+		void NewScene();
+		void OpenScene();
+		void SaveSceneAs();
+	private:
 		Fountain::OrthographicCameraController m_CameraController;
 
 		// Temp
