364595e81b808f7bbd934095fe72632df4978260
KirkXu <rjxu@mail.sdu.edu.cn>
2021-11-07 22:28:24 +0800

    Added missing 'SetFloat2' in Renderer for completeness

--- Fountain/src/Platform/OpenGL/OpenGLShader.cpp
+++ Fountain/src/Platform/OpenGL/OpenGLShader.cpp
@@ -214,6 +214,13 @@ namespace Fountain {
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		FT_PROFILE_FUNCTION();
+
+		UploadUniformFloat2(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		FT_PROFILE_FUNCTION();
