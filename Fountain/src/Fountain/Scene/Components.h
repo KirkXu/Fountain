f64cc0663df180840c3823c482dd3ca63fd78411
KirkXu <rjxu@mail.sdu.edu.cn>
2021-11-10 16:54:37 +0800

    Added texture drag/drop and tiling factor to Sprite Component

--- Fountain/src/Fountain/Scene/Components.h
+++ Fountain/src/Fountain/Scene/Components.h
@@ -8,6 +8,7 @@
 
 #include "SceneCamera.h"
 #include "ScriptableEntity.h"
+#include "Fountain/Renderer/Texture.h"
 
 namespace Fountain {
 
@@ -55,6 +56,8 @@ namespace Fountain {
 	struct SpriteRendererComponent
 	{
 		glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+		Ref<Texture2D> Texture;
+		float TilingFactor = 1.0f;
 
 		SpriteRendererComponent() = default;
 		SpriteRendererComponent(const SpriteRendererComponent&) = default;
