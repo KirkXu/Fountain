d26165607651fd6552e0088b4d2a4732f30bcd8d
KirkXu <rjxu@mail.sdu.edu.cn>
2021-11-11 20:19:29 +0800

    Added loging glm vectors, matrix and quaternios

--- Fountain/src/Fountain/Core/Log.h
+++ Fountain/src/Fountain/Core/Log.h
@@ -1,6 +1,10 @@
 #pragma once
 
+
 #include "Fountain/Core/Base.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
 
 // This ignores all warnings raised inside External headers
 #pragma warning(push, 0)
@@ -23,6 +27,24 @@ namespace Fountain {
 
 	};
 
+}
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::vec<L, T, Q>& vector)
+{
+	return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
+{
+	return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternio)
+{
+	return os << glm::to_string(quaternio);
 }
 
 // Core log marcos
